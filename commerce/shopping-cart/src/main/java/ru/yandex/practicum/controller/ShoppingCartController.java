808d84cb09f1031458d4325f7890977e8dd1ec16
yuliya azarkovich <3tochki@gmail.com>
2024-12-19 22:53:57 +0300

    shopping-cart

--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/controller/ShoppingCartController.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/controller/ShoppingCartController.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.dto.ChangeProductQuantityRequest;
 import ru.yandex.practicum.dto.ShoppingCartDto;
 import ru.yandex.practicum.service.ShoppingCartService;
 
@@ -18,5 +19,25 @@ public class ShoppingCartController {
     @PutMapping
     public ShoppingCartDto addProducts(@RequestParam String userName, @RequestBody Map<UUID, Integer> dto) {
         return service.addProducts(dto, userName);
+    }
+
+    @GetMapping
+    public ShoppingCartDto getShoppingCart(@RequestParam String userName) {
+        return service.getShoppingCart(userName);
+    }
+
+    @DeleteMapping
+    public void deleteShoppingCart(@RequestParam String userName) {
+        service.deactivateShoppingCart(userName);
+    }
+
+    @PostMapping("/remove")
+    public ShoppingCartDto removeProducts(@RequestParam String userName, @RequestBody Map<UUID, Integer> dto) {
+        return service.removeProducts(userName, dto);
+    }
+
+    @PostMapping("/change-quantity")
+    public ShoppingCartDto changeQuantityProducts(@RequestParam String userName, @RequestBody ChangeProductQuantityRequest request) {
+        return service.changeQuantityProducts(userName, request);
     }
 }
--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/service/ShoppingCartService.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/service/ShoppingCartService.java
@@ -1,11 +1,19 @@
 package ru.yandex.practicum.service;
 
+import jdk.jfr.StackTrace;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.dto.BookedProductsDto;
+import ru.yandex.practicum.dto.ChangeProductQuantityRequest;
 import ru.yandex.practicum.dto.ShoppingCartDto;
 import ru.yandex.practicum.dto.ShoppingCartStatus;
+import ru.yandex.practicum.exception.NoProductsInShoppingCartException;
+import ru.yandex.practicum.exception.ShoppingCartNotFoundException;
+import ru.yandex.practicum.mapper.BookedProductMapper;
 import ru.yandex.practicum.mapper.ShoppingCartMapper;
+import ru.yandex.practicum.model.BookedProduct;
 import ru.yandex.practicum.model.ShoppingCart;
+import ru.yandex.practicum.repository.BookedProductRepository;
 import ru.yandex.practicum.repository.ShoppingCartRepository;
 
 import java.util.Map;
@@ -17,7 +25,7 @@ import java.util.UUID;
 public class ShoppingCartService {
 
     private final ShoppingCartRepository repository;
-
+    private final BookedProductRepository bookedProductRepository;
     public ShoppingCartDto addProducts(Map<UUID, Integer> dto, String userName) {
         Optional<ShoppingCart> cart = repository.findByUserNameAndStatus(userName, ShoppingCartStatus.ACTIVE);
         ShoppingCart curCart;
@@ -35,4 +43,60 @@ public class ShoppingCartService {
 
         return ShoppingCartMapper.INSTANCE.shoppingCartToDto(curCart);
     }
+
+    public ShoppingCartDto getShoppingCart(String userName) {
+        Optional<ShoppingCart> cart = repository.findByUserNameAndStatus(userName, ShoppingCartStatus.ACTIVE);
+        if (cart.isEmpty()) {
+            return null;
+        }
+        return ShoppingCartMapper.INSTANCE.shoppingCartToDto(cart.get());
+    }
+
+    public void deactivateShoppingCart(String userName) {
+        Optional<ShoppingCart> cart = repository.findByUserNameAndStatus(userName, ShoppingCartStatus.ACTIVE);
+        if (cart.isEmpty()) {
+            throw new ShoppingCartNotFoundException("Корзина не найдена");
+        }
+        cart.get().setStatus(ShoppingCartStatus.DEACTIVATE);
+        repository.save(cart.get());
+    }
+
+    public ShoppingCartDto removeProducts(String userName, Map<UUID, Integer> products) {
+        Optional<ShoppingCart> cart = repository.findByUserNameAndStatus(userName, ShoppingCartStatus.ACTIVE);
+        if (cart.isEmpty()) {
+            throw new ShoppingCartNotFoundException("Корзина не найдена");
+        }
+        for (UUID u : products.keySet()) {
+
+            cart.get().products.remove(u);
+        }
+        return ShoppingCartMapper.INSTANCE.shoppingCartToDto(repository.save(cart.get()));
+    }
+
+    public ShoppingCartDto changeQuantityProducts(String userName, ChangeProductQuantityRequest request) {
+        Optional<ShoppingCart> cart = repository.findByUserNameAndStatus(userName, ShoppingCartStatus.ACTIVE);
+        if (cart.isEmpty()) {
+            throw new ShoppingCartNotFoundException("Корзина не найдена");
+        }
+        if (!cart.get().products.containsKey(request.getProductId())) {
+            throw new NoProductsInShoppingCartException("Товар не найден", new Throwable());
+        }
+        cart.get().products.put(request.getProductId(), request.getNewQuantity());
+
+        return ShoppingCartMapper.INSTANCE.shoppingCartToDto(repository.save(cart.get()));
+    }
+
+    public BookedProductsDto bookedProducts(String userName) {
+        Optional<ShoppingCart> cart = repository.findByUserNameAndStatus(userName, ShoppingCartStatus.ACTIVE);
+        if (cart.isEmpty()) {
+            throw new ShoppingCartNotFoundException("Корзина не найдена");
+        }
+        BookedProduct bookedProduct = new BookedProduct();
+        bookedProduct.setCart(cart.get());
+        bookedProduct.setFragile(true);
+        bookedProduct.setDeliveryVolume(0.0);
+        bookedProduct.setDeliveryWeight(0.0);
+        return BookedProductMapper.INSTANCE.bookedProductsToDto(bookedProductRepository.save(bookedProduct));
+    }
+
 }
